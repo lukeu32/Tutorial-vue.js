93ac8b5ddde6e04ae9321be2a2f734d75b4b7ae0
lukeu32 <lukasz.wajda@vp.pl>
2020-04-17 23:48:53 +0200

    Conditional Rendering

--- Socks website tutorial/main.js
+++ Socks website tutorial/main.js
@@ -2,7 +2,10 @@ var app= new Vue({
 	el: '#app', 
 	data: { 
 		product: 'Socks', //example of attribute
-		image: 'vmSocks-green-onWhite.png',
-		altText: 'A pair of socks'
+		image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',  // or 'vmSocks-green-onWhite.png',
+		altText: 'A pair of socks',
+		//inventory: 0,
+		inStock: true,
+		onSale: true
 	}
 })
