4f917ffac9e28b26cf655b4283e2baedee697545
lukeu32 <lukasz.wajda@vp.pl>
2020-04-17 22:43:52 +0200

    Attribute Binding

--- Socks website tutorial/main.js
+++ Socks website tutorial/main.js
@@ -1,6 +1,8 @@
-var app= new Vue({ //the vue instance. It is created by passing an options object into it.
-	el: '#app', //Instancja Vue jest następnie podłączana do wybranego elementu. Innymi słowy, aktywujemy Vue na div z identyfikatorem id appustawiając '``#app``'jako element ( el), do którego podłączona jest nasza instancja.
-	data: { //Instancja Vue ma miejsce na dane we swojej właściwości data.
-		product: 'Socks' //przykład deklaracji zniennej
+var app= new Vue({ 
+	el: '#app', 
+	data: { 
+		product: 'Socks', //example of attribute
+		image: 'vmSocks-green-onWhite.png',
+		altText: 'A pair of socks'
 	}
 })
