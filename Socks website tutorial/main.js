45d7d759ae63cefa504cacf1f17185ad3daf1f23
lukeu32 <lukasz.wajda@vp.pl>
2020-04-18 14:35:55 +0200

    Components

--- Socks website tutorial/main.js
+++ Socks website tutorial/main.js
@@ -1,6 +1,69 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+})
+
+Vue.component('product', { //The first argument is the name we choose for the component, and the second is an options object, similar to how we created our initial Vue instance.
+	
+	props: { //Props are essentially variables that are waiting to be filled with the data its parent sends down into it. 
+		premium: {
+			type: Boolean,
+			required: true
+		}
+	},// We are specifying what props the product component is expecting to receive by adding a props object to our component.
+	
+	template: `
+	  <div class="product">
+        
+    <div class="product-image">
+      <img :src="image" />
+    </div>
+
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <p>{{ sale }}</p>
+	  <p>Shipping: {{ shipping }}</p>
+
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+
+
+        <div class="color-box"
+             v-for="(variant, index) in variants" 
+             :key="variant.variantId"
+             :style="{ backgroundColor: variant.variantColor }"
+             @mouseover="updateProduct(index)" > <!--Now instead of passing in the variantImage, we’ll pass in the index. -->
+        </div> 
+
+        <button v-on:click="addToCart" 
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }"
+          >
+        Add to cart
+        </button>
+
+        <div class="cart">
+          <p>Cart({{ cart }})</p>
+        </div>
+
+      </div>  
+    
+  </div>
+	`,
+	data() { //Since data is now a function that returns a data object, each component will definitely have its own data. If data weren’t a function, each product component would be sharing the same data everywhere it was used, defeating the purpose of it being a reusable component.
+	return {
         product: 'Socks',
         brand: 'Vue Mastery',
         selectedVariant: 0, //Instead of having image in our data, let’s replace it with selectedVariant, which we’ll initialize as 0. Because we’ll be setting this based on the index that we hover on.
@@ -21,7 +84,9 @@ var app = new Vue({
         ],
         cart: 0,
         onSale: true
+	}
     },
+		
     methods: {
         addToCart: function() {
             this.cart += 1
@@ -47,13 +112,21 @@ var app = new Vue({
             return this.brand + ' ' + this.product + ' are on sale!'
           } 
             return  this.brand + ' ' + this.product + ' are not on sale'
-        }
+        },
+		shipping() {
+			if(this.premium) {
+				return "Free"
+			}
+			return 2.99
+		}
     }
+})
+	
+var app = new Vue({
+    el: '#app',
+	data: {
+		premium: true
+	}
+    
   })
   
-  
-  //Computed properties are cached, meaning the result is saved until its dependencies change. So when quantity changes, the cache will be cleared and the **next time you access the value of inStock , it will return a fresh result, and cache that result.
-
-  //With that in mind, it’s more efficient to use a computed property rather than a method for an expensive operation that you don’t want to re-run every time you access it.
-
-  //It is also important to remember that you should not be mutating your data model from within a computed property. You are merely computing values based on other values. Keep these functions pure.
