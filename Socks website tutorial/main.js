b81168d2e1c502c94462554e704f8d021458bcc8
lukeu32 <lukasz.wajda@vp.pl>
2020-04-18 11:16:53 +0200

    List Rendering

--- Socks website tutorial/main.js
+++ Socks website tutorial/main.js
@@ -3,9 +3,18 @@ var app= new Vue({
 	data: { 
 		product: 'Socks', //example of attribute
 		image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',  // or 'vmSocks-green-onWhite.png',
-		altText: 'A pair of socks',
-		//inventory: 0,
 		inStock: true,
-		onSale: true
-	}
+		details: ['80% cotton', '20% polyester', 'Gender-neutral'], //array of details
+	variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'    
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ],// still array of variants
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+  } 
 })
